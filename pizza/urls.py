c38f188558a2d0d35fab24964f2ed1f70e7658a6
Ege Hurturk <ege.hurturk@gmail.com>
2020-05-30 22:20:51 +0300

    added user app url to main urls.py

--- pizza/urls.py
+++ pizza/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path("", include("orders.urls")),
     path("admin/", admin.site.urls),
+    path("", include("users.urls")),
+    # path('accounts/login/', admin.site.urls),
 ]
