6f2555af27f5a32207370fd2afb99bbcc3be10ad
Ege Hurturk <ege.hurturk@gmail.com>
2020-05-31 21:19:28 +0300

    Docstring

--- users/forms.py
+++ users/forms.py
@@ -4,6 +4,14 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
+
+	""" 
+	User Registration Form - Default Django Form (Inherited from UserCreationForm) 
+
+	FIELDS: Username, Password (hashed), Email, First Name, Last Name/
+
+	"""
+
     email = forms.EmailField()
     first_name = forms.CharField(max_length=20)
     last_name = forms.CharField(max_length=20)
