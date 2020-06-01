19b2c3c14a87fbc5a5c2195848c2dfa0b056868e
Ege Hurturk <ege.hurturk@gmail.com>
2020-06-01 21:08:14 +0300

    jQuery Vanilla JS for dropdown

--- static/orders/js/app.js
+++ static/orders/js/app.js
@@ -1,8 +1,26 @@
-
-
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', ()=>{
+    const dropdown = $('#myDropdown');
+    const dropButton = $('.dropbtn');
+    const closeButton = $('closeNotification');
     const notification = $('#notificationsLog')
-    $('closeNotification').click(()=>{
-        notification.fadeOut()
-    })
+
+    // By default, dropdown menu is closed
+    dropdown.hide();
+
+    // When the profile button is clicked, open the menu
+    dropButton.click(()=> {
+        dropdown.fadeToggle();
+    });
+
+    // When it is focused out, close the menu.
+    dropButton.focusout(()=> {
+        dropdown.fadeOut();
+    });
+
+    closeButton.click(()=> {
+        console.log('clicked')
+        notification.fadeOut();
+    });
+
+
 })
